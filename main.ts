993a0ff026ab05c5f692bfb7509bfdcae5130666
VarunTQQber <122548038+VarunTQQber@users.noreply.github.com>
2023-01-23 11:20:27 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,43 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("How Are You?")
 })
+input.onGesture(Gesture.TiltLeft, function () {
+    if (sprite_on == 1) {
+        sprite.move(-1)
+    }
+    if (sprite.get(LedSpriteProperty.X) == 0) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    } else if (sprite.get(LedSpriteProperty.X) == 1) {
+        music.playTone(294, music.beat(BeatFraction.Whole))
+    } else if (sprite.get(LedSpriteProperty.X) == 2) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
+    } else if (sprite.get(LedSpriteProperty.X) == 3) {
+        music.playTone(349, music.beat(BeatFraction.Whole))
+    } else {
+        music.playTone(392, music.beat(BeatFraction.Whole))
+    }
+})
+input.onPinPressed(TouchPin.P2, function () {
+    sprite_on = 1
+    sprite = game.createSprite(2, 2)
+})
+input.onButtonPressed(Button.AB, function () {
+    coin = randint(0, 1)
+    for (let index = 0; index < 8; index++) {
+        basic.showString("H",10)
+basic.showString("T",10)
+    }
+    for (let index = 0; index < 3; index++) {
+        basic.showString("H", 50)
+basic.showString("T", 50)
+    }
+    basic.showString(" ",1)
+if (coin == 0) {
+        basic.showString("Heads!")
+    } else {
+        basic.showString("Tails!")
+    }
+})
 input.onButtonPressed(Button.B, function () {
     if (oddOrEven == 0) {
         oddOrEven = 1
@@ -11,8 +48,7 @@ input.onButtonPressed(Button.B, function () {
             # . . . #
             . # # # .
             `)
-    }
-    if (oddOrEven == 1) {
+    } else {
         oddOrEven = 0
         basic.showLeds(`
             . . . . .
@@ -23,9 +59,27 @@ input.onButtonPressed(Button.B, function () {
             `)
     }
 })
+input.onPinPressed(TouchPin.P1, function () {
+    sprite_on = 0
+    sprite.delete()
+})
 input.onGesture(Gesture.Shake, function () {
-    dice = randint(1, 6)
     music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+    for (let index = 0; index < 4; index++) {
+        basic.showString("1", 10)
+basic.showString("2", 10)
+basic.showString("3", 10)
+basic.showString("4", 10)
+basic.showString("5", 10)
+basic.showLeds(`
+        . # # # .
+        # . . . .
+        # # # # .
+        # . . . #
+        . # # # .
+        `, 10)
+    }
+    dice = randint(1, 6)
     if (dice == 1) {
         basic.showString("1")
     } else if (dice == 2) {
@@ -37,18 +91,52 @@ input.onGesture(Gesture.Shake, function () {
     } else if (dice == 5) {
         basic.showString("5")
     } else {
-        basic.showString("6")
+        basic.showLeds(`
+            . # # # .
+            # . . . .
+            # # # # .
+            # . . . #
+            . # # # .
+            `)
+    }
+})
+input.onGesture(Gesture.TiltRight, function () {
+    if (sprite_on == 1) {
+        sprite.move(1)
+    }
+    if (sprite.get(LedSpriteProperty.X) == 0) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    } else if (sprite.get(LedSpriteProperty.X) == 1) {
+        music.playTone(294, music.beat(BeatFraction.Whole))
+    } else if (sprite.get(LedSpriteProperty.X) == 2) {
+        music.playTone(330, music.beat(BeatFraction.Whole))
+    } else if (sprite.get(LedSpriteProperty.X) == 3) {
+        music.playTone(349, music.beat(BeatFraction.Whole))
+    } else {
+        music.playTone(392, music.beat(BeatFraction.Whole))
     }
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
     music.playMelody("E E F G G F E D ", 120)
     music.playMelody("C C D E E D D - ", 120)
     music.playMelody("E E F G G F E D ", 120)
     music.playMelody("C C D E D C C - ", 120)
 })
 let dice = 0
+let coin = 0
+let sprite: game.LedSprite = null
 let oddOrEven = 0
+let sprite_on = 0
+sprite_on = 0
 oddOrEven = 0
+basic.showString("FIDGET! pin 2 = game")
 basic.forever(function () {
 	
 })
